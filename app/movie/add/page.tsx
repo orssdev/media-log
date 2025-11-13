d9636a1391d4b96b4b2934c53712fd03c7688580
Oscar <orss.dev@gmail.com>
2025-11-12 23:15:39 -0500

    Horizontal Scrolling

--- app/movie/add/page.tsx
+++ app/movie/add/page.tsx
@@ -29,11 +29,11 @@ export default function AddMovie() {
     }
 
     return (
-        <div className="w-full min-h-[800px] h-auto bg-(--color1) bg-linear-to-b from-[#000000] to-[#210304] flex flex-col items-center py-[100px] gap-[50px] font-e">
+        <div className="w-full min-h-[800px] h-auto bg-(--color1) bg-linear-to-b from-[#000000] to-[#210304] flex flex-col items-center py-[50px] gap-10 font-e">
             <MovieForm query={query} setQuery={setQuery} onSearch={handleSearch}/>
             {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
-            <div className="flex justify-evenly flex-wrap gap-[50px] p-[25px]">
+            <div className="flex flex-row overflow-x-auto gap-[25px] px-[25px] w-full">
                 {movies.map(movie => (
                     <MovieCard key={movie.id} movie={movie}/>
                 ))}
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,7 +5,7 @@ export default function MovieCard({movie}: {movie: any}){
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
     : '/no-image-342x513.png';
     return (
-        <div className="w-[342px] h-[600px] bg-(--color3) rounded-2xl overflow-hidden text-black border-4 border-(--color2)">
+        <div className="w-[342px] h-[600px] bg-(--color3) rounded-2xl overflow-hidden text-black border-4 border-(--color2) shrink-0">
             {/* <div className="w-full h-[278px]"></div> */}
             <img src={imageUrl} alt={movie.title} className="object-cover" />
             <div className="p-2.5">
